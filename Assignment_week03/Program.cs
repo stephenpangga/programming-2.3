910a2c5da520931abd3e4d615f2219918cf73414
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-03-09 00:30:08 +0100

    working on assignment 1 question b

--- Assignment_week03/Program.cs
+++ Assignment_week03/Program.cs
@@ -10,19 +10,21 @@ namespace Program01
         {
             Program myProgram = new Program();
             myProgram.Start();
-            Console.ReadLine();
+            Console.ReadKey();
             
         }
         void Start()
         {
             Course course = ReadCourse("Enter a course.");
-
-
+            //DisplayPracticalGrade();
+            DisplayCourse(course);
         }
 
         PracticalGrade ReadPracticalGrade(string question)
         {
-
+            string practical = ReadString(question);
+            int Pgrade = int.Parse(practical);
+            return(PracticalGrade)Pgrade;
         }
         
         void DisplayPracticalGrade(PracticalGrade Practical)
@@ -32,21 +34,35 @@ namespace Program01
 
         Course ReadCourse(string question)
         {
+            Course course = new Course();
             Console.WriteLine(question);
-            Console.Write("Name of the course: ");
-
-
+            course.name = ReadString("Name of the course: ");
+            course.Grade = ReadInt($"Grade for {course.name}: ", 0, 100);
+            Console.WriteLine("0. None 1. Abset 2. Insufficient 3. Sufficient 4. Good");
+            course.PracticalGrade = ReadPracticalGrade($"Pratical grade for {course.name}: ");
+            return course;        
         }
 
-        void DisplayCourse()
+        void DisplayCourse(Course course)
         {
-
+            Console.WriteLine($"{course.name} : {course.Grade}     {course.PracticalGrade}");
         }
 
         int ReadInt(string question)
         {
             Console.Write(question);
             return int.Parse(Console.ReadLine());
+        }
+
+        int ReadInt(string question, int min, int max)
+        {
+            int grade = 0;
+            do
+            {
+                grade = ReadInt(question);
+            }
+            while (grade < min || grade > max);
+            return grade;
         }
 
         string ReadString(string question)
