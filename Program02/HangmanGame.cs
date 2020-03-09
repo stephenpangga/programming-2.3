9e59d39d06398463b1356bc1715a5164f2910b96
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-03-09 17:24:43 +0100

    Working on Assignment02

---
+++ Program02/HangmanGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program02
+{
+    class HangmanGame
+    {
+        public string secretWord;
+        public string guessedWord;
+
+        public void Init(string secretWord)
+        {
+            foreach (char letter in secretWord)
+            {
+                this.secretWord = secretWord;//just to show the word according to jurek.
+                guessedWord += ".";
+            }
+        }
+    }
+}
---
+++ Program02/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+            Console.ReadKey();
+        }
+
+        void Start()
+        {
+            //question B
+            List<string> words = new List<string>();
+            words = ListOfWords();
+            //choosing the word.
+            SelectWord(words);
+
+            HangmanGame hangman = new HangmanGame();
+
+            //hangman.Init("backdoor");
+            hangman.Init(SelectWord(words));
+            Console.WriteLine("The secret word is: " + hangman.secretWord);
+            Console.WriteLine("The guessed word is: " + hangman.guessedWord);
+
+            PlayHangman(hangman);
+
+        }
+
+        List<string> ListOfWords()
+        {
+            List<string> Lwords = new List<string>();
+            Lwords.Add("Regina");
+            Lwords.Add("Stephen");
+            Lwords.Add("Sisa");
+            Lwords.Add("Eleanor");
+            Lwords.Add("Lola");
+            Lwords.Add("Amy");
+            Lwords.Add("Bebe");
+            Lwords.Add("Roo");
+            return Lwords;
+        }
+
+        string SelectWord(List<string> words)
+        {
+            //randomizer
+            Random rnd = new Random();
+            int value = rnd.Next(0, words.Count);
+            string randomWord = words.ElementAt(value);
+            return randomWord;
+        }
+
+        bool PlayHangman(HangmanGame hangman)
+        {
+            //Letter Storage
+            List<char> enteredLetters = new List<char>();
+            DisplayWord(hangman.guessedWord);
+            //word guessed?
+            return true;
+        }
+
+        void DisplayWord(string word)
+        {
+            string secret = "";
+            foreach(char c in word)
+            {
+                secret = secret + c + " ";
+            }
+
+            Console.WriteLine($"The Secret word is: {secret}");
+        }
+
+    }
+}
